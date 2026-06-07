6db234022a16f6f9123cebd22f81e3d109dc51d8
ThoomAbhi <thoomabhisathwika@gmail.com>
2026-06-07 17:46:44 +0530

    changes

--- langchain_helper.py
+++ langchain_helper.py
@@ -47,7 +47,7 @@ def get_few_shot_db_chain():
         }
     },
     sample_rows_in_table_info=3)
-    llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=os.environ["GOOGLE_API_KEY"], temperature=0.1)
+    llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", google_api_key=os.environ["GOOGLE_API_KEY"], temperature=0.1)
 
     embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
     to_vectorize = [" ".join(example.values()) for example in few_shots]
